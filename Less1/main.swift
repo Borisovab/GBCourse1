7f844c3351c5f52ac7592d48d40bc48673c42ad3
Borisovab <borisovab@mail.ru>
2021-09-12 18:59:04 +0300

    Первое домашнее задание готово.

--- Less1/main.swift
+++ Less1/main.swift
@@ -1,11 +1,26 @@
-//
-//  main.swift
-//  Less1
-//
-//  Created by Александр Борисов on 12.09.2021.
-//
+// MARK: - Задача 1. Решить квадратное уравнение. Пример: 3x(2) - 14x - 5 = 0
+
+let a = 3
+let b = -14
+let c = -5
+var x1 = Double()
+var x2 = Double()
 
 import Foundation
 
-print("Hello, World!")
+let d = pow(Double(b), 2) - Double(4 * a * c)
+
+if (d > 0) {
+    x1 = Double((-b) + Int(sqrt(d))) / Double(2 * a)
+    x2 = Double((-b) - Int(sqrt(d))) / Double(2 * a)
+    print("Уравнение имеет два корня:")
+    print(x1)
+    print(x2)
+} else if (d == 0) {
+    x1 = Double((-b) / (2 * a))
+    print("Уравнение имеет один корень:")
+    print(x1)
+} else {
+    print("Корней нет")
+}
 
